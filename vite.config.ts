26e465bc1f0d9803b99d3968526eb1b1294ec01d
Troupix <maximilien.tordeux@gmail.com>
2025-10-24 13:47:32 +0200

    chore(ci): set output dir from vite as build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/Githubdeploymentdashboard/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
